2dd481c45e73b74f62f4f6cc6f75a8bc24ba6d5a
Pablo Derendinger <pderendinger@gmail.com>
2020-06-17 11:39:48 -0300

    retrieving Models sdb files

--- src/forge/__init__.py
+++ src/forge/__init__.py
@@ -1,7 +1,10 @@
 import requests
 import json
+from glob import iglob
+import shutil
+import os
 
-credentials_file = "src\\Credentials.txt"
+credentials_file = "src\Credentials.txt"
 base_url = 'https://developer.api.autodesk.com/'
 access_token = None
 current_hub_name = "ADN" 
@@ -121,18 +124,18 @@ def getFolderContent(folderId, projectId, access_token):
     content = json.loads(response.text.encode('utf8'))
     return content['data']
 
-def travelFolders(folderId , projectId, access_token, result):
+def travelFoldersForRvts(folderId , projectId, access_token, result):
     content = getFolderContent(folderId, projectId, access_token)
     for c  in content:
         if c['type'] == 'folders':
-            travelFolders(c['id'], projectId, access_token, result)
+            travelFoldersForRvts(c['id'], projectId, access_token, result)
         elif (c['type'] == 'items' 
         and ".rvt"  in c['attributes']['displayName']
         ): # and '.rvt' in c['attributes']['displayName']
             result.append(c)
 
 def get_topfolders(hub_id,project_id,access_token):
-    url= "https://developer.api.autodesk.com/project/v1/hubs/%s/projects/%s/topFolders" % (hub_id, project_id)
+    url= base_url + "/project/v1/hubs/%s/projects/%s/topFolders" % (hub_id, project_id)
     payload = {}
     headers = {
         'Authorization': 'Bearer %s' % access_token
@@ -144,7 +147,92 @@ def get_topfolders(hub_id,project_id,access_token):
         if folder['attributes']['displayName']=="Project Files":
             return folder["id"]
 
+def getItem(project_id, item_id, access_token):
+    url = base_url + '/data/v1/projects/%s/items/%s' % (project_id, item_id['id'])
+    payload = {}
+    headers = {
+        'Authorization': 'Bearer %s' % access_token
+    }
+    response = requests.get( url, headers=headers, data = payload)
+    content = json.loads(response.text.encode('utf8'))
+    return content['included'][0]['relationships']['derivatives']['data']['id']
+
+def getDerivativeUrn(urn, access_token):
+    url = base_url + '/modelderivative/v2/designdata/%s/manifest' % urn
+    payload = {}
+    headers = {
+        'Authorization': 'Bearer %s' % access_token
+    }
+    response = requests.get( url, headers=headers, data = payload)
+    content = json.loads(response.text.encode('utf8'))
+    for c in content['derivatives'][0]['children']:
+        if c['mime'] == 'application/autodesk-db':
+            return c['urn']
+    return None
+
+def createChunks(content):
+
+    limit  = 20000000
+    if content < limit:
+        return ['bytes=0-%s' % content]
+    
+    previous = 0
+    output = []
+    for s in range(limit,content, limit):
+        if s> content:
+            s = content
+        _range = 'bytes=%s-%s' % (previous,s)
+        output.append(_range)
+        previous = s+1
+
+    _range = 'bytes=%s-%s' % (previous,content)
+    output.append(_range)
+    return output
+
+def downloadDerivative(urn,derivativeUrn, access_token, projectId):
+    
+    url = base_url+  '/modelderivative/v2/designdata/%s/manifest/%s' % (urn, derivativeUrn)
+    headers = {
+        'Authorization': 'Bearer %s' % access_token
+    }
+    response = requests.head( url, headers=headers)
+
+    if response.status_code != 200:
+        print("Error retrieving derivative heads")
+        return None
+    content = int(response.headers._store['content-length'][1])
+    chunks = createChunks(content)
+
+    with requests.Session() as session:
+        for i, c in enumerate(chunks):
+            url = base_url+  '/modelderivative/v2/designdata/%s/manifest/%s' % (urn, derivativeUrn)
+            headers = {
+                'Authorization': 'Bearer %s' % access_token,
+                'Range': c
+            }
+            response = requests.get( url, headers=headers)
+
+            if response.status_code != 200:
+                with open('db\\%s.part' % i, 'wb') as f:
+                    f.write(response.content)
+
+
+    #Join parts
+    destinyPath = 'db\\'
+    destinyDB = r'db\\%s.sdb' % projectId.replace("urn:adsk.wipprod:dm.lineage:","")
+    destination = open(destinyDB, 'wb')
+    for filename in iglob(os.path.join(destinyPath, '*.part')):
+        shutil.copyfileobj(open(filename, 'rb'), destination)
+        os.remove(filename)
+    destination.close()
+
+
+
 def getRevitFiles( projectfolder, projectId, access_token):
     content = []
-    travelFolders(projectfolder, projectId, access_token, content)
-    print(content)
+    access_token = Authenticate()
+    travelFoldersForRvts(projectfolder, projectId, access_token, content)
+    for model in content:
+        urn  = getItem(projectId, model, access_token)
+        dbUrn = getDerivativeUrn(urn, access_token)
+        downloadDerivative(urn, dbUrn, access_token, model['id'])
