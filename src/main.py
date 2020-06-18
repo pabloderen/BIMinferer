a69fb52224b78218bed1937ce7f117996dc34823
Pablo Derendinger <pderendinger@gmail.com>
2020-06-18 09:23:35 -0300

    refactor code

--- src/main.py
+++ src/main.py
@@ -1,14 +1,6 @@
 
-from forge import Authenticate, getHub, getProjects, getRevitFiles,get_topfolders
+from forge import getDBFilesFromHub
 
 
 if __name__ == '__main__':
-    access_token = Authenticate()
-    if access_token:
-        hubId = getHub(access_token, 'ADN')
-        if hubId:
-            projects = getProjects(hubId)
-            for project in projects:
-                projectfolder=get_topfolders(hubId,project,access_token)
-                getRevitFiles(projectfolder, project, access_token)
-                
+    getDBFilesFromHub()
